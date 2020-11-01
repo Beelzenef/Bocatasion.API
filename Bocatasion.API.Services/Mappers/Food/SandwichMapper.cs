7d852810a1b6df5481ce86b2b09865b9c7c3ec4e
Elena G <elena.guzbla@gmail.com>
2020-11-01 19:00:02 +0100

    ✨ Add main service
    Also mapping, references and fake data generation

---
+++ Bocatasion.API.Services/Mappers/Food/SandwichMapper.cs
@@ -0,0 +1,29 @@
+﻿using Bocatasion.API.Contracts.DTOs;
+using Bocatasion.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bocatasion.API.Services.Mappers
+{
+    public static class SandwichMapper
+    {
+        public static SandwichDto MapToSandwichDto(SandwichModel model)
+        {
+            var dto = new SandwichDto
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Disabled = model.Disabled,
+                ImageUrl = model.ImageUrl,
+                Price = model.Price
+            };
+
+            return dto;
+        }
+
+        public static IEnumerable<SandwichDto> MapToSandwichDtoList(List<SandwichModel> models)
+        {
+            return models.Select(MapToSandwichDto);
+        }
+    }
+}
---
+++ Bocatasion.API.Services/Services/Food/SandwichService.cs
@@ -0,0 +1,26 @@
+﻿using Bocatasion.API.Contracts.DTOs;
+using Bocatasion.API.QA;
+using Bocatasion.API.Services.Mappers;
+using System.Collections.Generic;
+
+namespace Bocatasion.API.Services
+{
+    public class SandwichService
+    {
+        public SandwichService()
+        {
+
+        }
+
+        public IEnumerable<SandwichDto> GetAllSandwiches()
+        {
+            var sandwichModels = SandwichBuilder.GenerateSandwichModels();
+
+            var dtos = SandwichMapper.MapToSandwichDtoList(sandwichModels);
+
+            return dtos;
+        }
+
+        
+    }
+}
