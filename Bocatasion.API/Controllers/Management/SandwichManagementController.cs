3258ceccf663276f5b99f1ad122bdc3bcd7a0c79
Elena G <elena.guzbla@gmail.com>
2020-11-01 19:01:49 +0100

    🎨 Refactor main controller and change launch settings

---
+++ Bocatasion.API/Controllers/Management/SandwichManagementController.cs
@@ -0,0 +1,28 @@
+﻿using Bocatasion.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bocatasion.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SandwichManagementController : ControllerBase
+    {
+        private readonly ILogger<SandwichManagementController> _logger;
+        private readonly SandwichService _sandwichService;
+
+        public SandwichManagementController(ILogger<SandwichManagementController> logger)
+        {
+            _logger = logger;
+            _sandwichService = new SandwichService();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllSandwiches()
+        {
+            var sandwiches = _sandwichService.GetAllSandwiches();
+
+            return Ok(sandwiches);
+        }
+    }
+}
