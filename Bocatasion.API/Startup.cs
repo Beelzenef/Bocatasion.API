0bdcffd5ae2752b2fb8b95743648289b31389aa7
Elena G <elena.guzbla@gmail.com>
2020-11-04 08:56:17 +0100

    🏗 Add Swagger

--- Bocatasion.API/Startup.cs
+++ Bocatasion.API/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Bocatasion.API
 {
@@ -32,6 +36,23 @@ namespace Bocatasion.API
                 c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
             });
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Bocatasion.API",
+                    Description = "... porque no se hace sola, hay que hacerla",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Elena G",
+                        Email = "elena.guzbla@gmail.com",
+                        Url = new Uri("https://twitter.com/Beelzenef_"),
+                    }
+                });
+                
+            });
+
             services.AddDbContext<Context>(item =>
                 item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<IDatabaseContext, Context>();
@@ -48,6 +69,13 @@ namespace Bocatasion.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bocatasion.API V1");
+            });
 
             app.UseCors(options => options.AllowAnyOrigin());
             app.UseHttpsRedirection();
