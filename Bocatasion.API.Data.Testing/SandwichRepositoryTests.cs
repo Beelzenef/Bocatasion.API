bee80027b38631348b34624338d3d5a39db71195
Elena G <elena.guzbla@gmail.com>
2020-11-28 21:17:51 +0100

    ✅ Add sandwich repo tests

---
+++ Bocatasion.API.Data.Testing/SandwichRepositoryTests.cs
@@ -0,0 +1,267 @@
+using Bocatasion.API.Bocatasion.API.Data;
+using Bocatasion.API.Bocatasion.API.Data.Repositories;
+using Bocatasion.API.Data.Contracts.Entities;
+using Bocatasion.API.QA;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Bocatasion.API.Data.Testing
+{
+    public class SandwichRepositoryTests
+    {
+        private Context _mockedContext;
+
+        public SandwichRepositoryTests()
+        {
+            _mockedContext = GetMockContext();
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_GetAll_Success()
+        {
+            // Arrange
+            var models = SandwichBuilder.BuildValidSandwichCollection();
+
+            _mockedContext.AddRange(models);
+            _mockedContext.SaveChanges();
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act
+            var result = repo.GetAll();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(models.Count());
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_GetById_Success()
+        {
+            // Arrange
+            int id = 2;
+            var model = SandwichBuilder.BuildValidSandwich(id);
+
+            _mockedContext.Add(model);
+            _mockedContext.SaveChanges();
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act
+            var result = repo.GetById(model.Id);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Id.Should().NotBe(0);
+            result.Id.Should().Be(model.Id);
+
+            // Clean up
+            CleanUp();
+        }
+
+
+        [Fact]
+        public void SandwichRepo_GetById_IdIsZero_ShouldFail()
+        {
+            // Arrange
+            var model = SandwichBuilder.BuildValidSandwich(0);
+
+            _mockedContext.Add(model);
+            _mockedContext.SaveChanges();
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            Sandwich result = null;
+
+            // Act - Assert
+            Assert.Throws<ArgumentNullException>(()
+                => result = repo.GetById(0));
+
+            result.Should().BeNull();
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Delete_Success()
+        {
+            // Arrange
+            int id = 2;
+            var model = SandwichBuilder.BuildValidSandwich(id);
+
+            _mockedContext.Add(model);
+            _mockedContext.SaveChanges();
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act
+            repo.Delete(model.Id);
+            repo.Save();
+
+            // Assert
+            var models = repo.GetAll();
+            models.Should().HaveCount(0);
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Delete_IdIsZero_ShouldFail()
+        {
+            // Arrange
+            var model = SandwichBuilder.BuildValidSandwich(0);
+
+            _mockedContext.Add(model);
+            _mockedContext.SaveChanges();
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act - Assert
+            Assert.Throws<ArgumentNullException>(()
+                => repo.Delete(0));
+
+            var models = repo.GetAll();
+            models.Should().HaveCount(1);
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Delete_EntityNotExists_ShouldFail()
+        {
+            // Arrange
+            var model = SandwichBuilder.BuildValidSandwich(1);
+
+            _mockedContext.Add(model);
+            _mockedContext.SaveChanges();
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act - Assert
+            repo.Delete(2);
+
+            var models = repo.GetAll();
+            models.Should().HaveCount(1);
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Insert_Success()
+        {
+            // Arrange
+            var model = SandwichBuilder.BuildValidSandwich(1);
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act 
+            repo.Insert(model);
+            repo.Save();
+
+            // Assert
+            var models = repo.GetAll();
+            models.Should().HaveCount(1);
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Insert_EntityIsNull_Success()
+        {
+            // Arrange
+            Sandwich model = null;
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act - Assert
+            Assert.Throws<ArgumentNullException>(()
+                => repo.Insert(model));
+
+            var models = repo.GetAll();
+            models.Should().HaveCount(0);
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Update_Success()
+        {
+            // Arrange
+            var model = SandwichBuilder.BuildValidSandwich(1);
+
+            _mockedContext.Add(model);
+            _mockedContext.SaveChanges();
+
+            string newDescription = "New description";
+
+            model.Description = newDescription;
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act 
+            repo.Update(model);
+            repo.Save();
+
+            // Assert
+            var models = repo.GetAll();
+            models.Should().HaveCount(1);
+            var updatedModel = repo.GetById(model.Id);
+            updatedModel.Description.Should().Be(newDescription);
+
+            // Clean up
+            CleanUp();
+        }
+
+        [Fact]
+        public void SandwichRepo_Update_EntityIsNull_ShouldFail()
+        {
+            // Arrange
+            Sandwich model = null;
+
+            var repo = new SandwichRepository(_mockedContext);
+
+            // Act - Assert
+            Assert.Throws<ArgumentNullException>(()
+                => repo.Update(model));
+
+            // Clean up
+            CleanUp();
+        }
+
+        private Context GetMockContext()
+        {
+            var models = SandwichBuilder.BuildValidSandwichCollection();
+
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: "Bocatasion")
+                .Options;
+
+            var context = new Context(options);
+            context.AddRange(models);
+            context.SaveChanges();
+
+            return context;
+        }
+
+        private void CleanUp()
+        {
+            var models = _mockedContext.Sandwiches;
+
+            _mockedContext.RemoveRange(models);
+            _mockedContext.SaveChanges();
+        }
+    }
+}
--- Bocatasion.API.QA/Food/SandwichBuilder.cs
+++ Bocatasion.API.QA/Food/SandwichBuilder.cs
@@ -1,31 +1,55 @@
-﻿using Bocatasion.API.Models;
+﻿using Bocatasion.API.Data.Contracts.Entities;
+using Bocatasion.API.Models;
+using Bogus;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bocatasion.API.QA
 {
     public static class SandwichBuilder
     {
-        public static List<SandwichModel> GenerateSandwichModels()
+        public static SandwichModel BuildValidSandwichModel()
         {
-            return new List<SandwichModel>
-            {
-                new SandwichModel
-                {
-                    Name = "Sandwich mixto",
-                    Description = "Delicious sandwich de jamón y queso fundido",
-                    Disabled = true,
-                    Price = 3.50,
-                    ImageUrl = "https://i-ticketing.iwos.com/256x256-th/products/213/products_213_27.jpg"
-                },
-                new SandwichModel
-                {
-                    Name = "Hot dog",
-                    Description = "Pan de perrito con salchica, a elegir con ketchup, mayonesa y/o mostaza",
-                    Disabled = false,
-                    Price = 4,
-                    ImageUrl = "https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/hot-dog.png"
-                }
-            };
+            Faker<SandwichModel> fake = new Faker<SandwichModel>()
+                .CustomInstantiator(_ => new SandwichModel())
+                .RuleFor(v => v.Id, f => f.Random.Int())
+                .RuleFor(v => v.Name, f => f.Random.String2(10))
+                .RuleFor(v => v.Description, f => f.Random.String2(20))
+                .RuleFor(v => v.Disabled, f => f.Random.Bool())
+                .RuleFor(v => v.ImageUrl, f => f.Internet.Url())
+                .RuleFor(v => v.Price, f => f.Random.Int());
+
+            return fake.Generate();
+        }
+
+        public static IEnumerable<SandwichModel> BuildSandwichModelCollection(int? count = 3)
+        {
+            Faker faker = new Faker();
+            IEnumerable<SandwichModel> items = faker.Make(count.Value, () => BuildValidSandwichModel());
+
+            return items.ToList();
+        }
+
+        public static Sandwich BuildValidSandwich(int id)
+        {
+            Faker<Sandwich> fake = new Faker<Sandwich>()
+                .CustomInstantiator(_ => new Sandwich())
+                .RuleFor(v => v.Id, f => id == 0 ? f.Random.Int(): id)
+                .RuleFor(v => v.Name, f => f.Random.String2(10))
+                .RuleFor(v => v.Description, f => f.Random.String2(20))
+                .RuleFor(v => v.Disabled, f => f.Random.Bool())
+                .RuleFor(v => v.ImageUrl, f => f.Internet.Url())
+                .RuleFor(v => v.Price, f => f.Random.Int());
+
+            return fake.Generate();
+        }
+
+        public static IEnumerable<Sandwich> BuildValidSandwichCollection(int? count = 3)
+        {
+            Faker faker = new Faker();
+            IEnumerable<Sandwich> items = faker.Make(count.Value, () => BuildValidSandwich(0));
+
+            return items.ToList();
         }
     }
 }
