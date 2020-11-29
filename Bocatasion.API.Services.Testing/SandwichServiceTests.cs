766207e7f43364c8d59b4453630cd0e4faed3ff0
Elena G <elena.guzbla@gmail.com>
2020-11-29 11:10:51 +0100

    ➕ Add service testing project + NuGets

---
+++ Bocatasion.API.Services.Testing/SandwichServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bocatasion.API.Services.Testing
+{
+    public class SandwichServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
